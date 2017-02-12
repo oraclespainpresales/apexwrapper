74126638ea89f627ca95e822162b56a0b7c04ece
Carlos Casares <ccasares@gmail.com>
2017-02-12 20:01:10 +0100

    add CORS support

--- server.js
+++ server.js
@@ -49,6 +49,11 @@ const PORT = 9997;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // REST stuff - BEGIN
 router.use(function(_req, _res, next) {
